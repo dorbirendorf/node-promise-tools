a41e0f55dcd117acc3d0c0447ed6b30742dfd41b
dor birendorf <dorbiren@gmail.com>
2021-12-25 15:44:34 +0200

    bla

--- src/index.ts
+++ src/index.ts
@@ -6,3 +6,5 @@ export { mapParallel, mapSeries } from "./map.js";
 export { filterParallel, filterSeries } from "./filter.js";
 export { reduce } from "./reduce.js";
 export { race, some } from "./race.js";
+
+console.log("live");
